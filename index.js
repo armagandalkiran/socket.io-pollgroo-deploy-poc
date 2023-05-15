5a2a0c401f408a6e5fb3392eead18960973dbeff
Armağan Dalkıran <armagandalkran20@gmail.com>
2023-05-15 22:37:32 +0300

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const socketIO = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const server = app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(cors());
+
+const server = app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
 
-const io = socketIO(server);
+const io = socketIO(server, {
+  cors: {
+    origin: "https://pollgroo-web-git-plgr-90-trendyol-web-team.vercel.app/",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected");
@@ -23,4 +32,13 @@ io.on("connection", (socket) => {
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/data", (req, res) => {
+  const data = {
+    message: "Test request!",
+    timestamp: new Date().toISOString(),
+  };
+
+  res.json(data);
 });
